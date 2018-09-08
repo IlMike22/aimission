5b929e33f3547732228f8eba59bbc702b6b09bb5
Michael Widlok <michaelwidlok@yahoo.de>
2018-09-08 11:47:39 +0200

    added aim list scene and fragments

---
+++ app/src/main/java/com/example/michl/aimission/AimListScene/Views/AimListActivity.kt
@@ -0,0 +1,13 @@
+package com.example.michl.aimission.AimListScene.Views
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.michl.aimission.R
+
+class AimListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_aim_list)
+    }
+}
--- app/src/main/java/com/example/michl/aimission/MainScene/Views/MainFragment.kt
+++ app/src/main/java/com/example/michl/aimission/MainScene/Views/MainFragment.kt
@@ -1,13 +1,11 @@
 package com.example.michl.aimission.MainScene.Views
 
 import android.content.Context
-import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
 import com.example.michl.aimission.R
 
 class MainFragment : Fragment() {
